a626acc5654870c646725fe9f229d583917f71c3
danlevil <danlevil@espol.edu.ec>
2025-01-28 18:06:29 -0500

    feat: Modificando las cards de respuesta faltantes y los charts

--- main/views.py
+++ main/views.py
@@ -26,11 +26,28 @@ def index(request):
     # Respuestas totales
     total_responses = len(response_dict.keys())
     # Valores de la respuesta
-    responses = response_dict.values()
+    responses = list(response_dict.values())
+    first_response = '{}\n{}'.format(responses[0]['email'].split("@")[0], responses[0]['albumfavorito'])
+    last_response = '{}\n{}'.format(responses[-1]['email'].split("@")[0], responses[-1]['albumfavorito'])
+    days_dict = {}
+    for entry in responses:
+        date = entry['saved'].split(',')[0]
+        days_dict[date] = days_dict.get(date, 0) + 1
 
+    highest_day = None
+    highest_count = -1
+    for key in list(days_dict.keys()):
+        response_count = days_dict[key]
+        if response_count > highest_count:
+            highest_day = key
+            highest_count = response_count
     data = {
         'title': 'Landing - Dashboard',
         'total_responses' : total_responses,
-        'responses' : responses
+        'responses' : responses,
+        'first_response' : first_response,
+        'last_response' : last_response,
+        'highest_day': highest_day
+
     }
     return render(request, 'main/index.html', data)
