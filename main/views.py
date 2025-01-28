1460c12d6475fef269e49351f38527a8cd1a187e
danlevil <danlevil@espol.edu.ec>
2025-01-28 11:32:51 -0500

    feat: Login y creacion de usuarios en django administration

--- main/views.py
+++ main/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required, permission_required
+
+@login_required
+@permission_required('main.index_viewer', raise_exception=True)
 
 def index(request):
-    #return HttpResponse("Hello, World!")
-    #return render(request, 'main/base.html')
-    return render(request, 'main/index.html')
+    # Arme el endpoint del REST API
+    current_url = request.build_absolute_uri()
+    url = current_url + '/api/v1/landing'
+
+    # Petición al REST API
+    response_http = requests.get(url)
+    response_dict = json.loads(response_http.content)
+
+    print("Endpoint ", url)
+    print("Response ", response_dict)
+    
+
+
+    # Respuestas totales
+    total_responses = len(response_dict.keys())
+    # Valores de la respuesta
+    responses = response_dict.values()
+
+    data = {
+        'title': 'Landing - Dashboard',
+        'total_responses' : total_responses,
+        'responses' : responses
+    }
+    return render(request, 'main/index.html', data)
